287aac0368dc068887ffc5cfa11dbc28947c6d4d
ChenBin12138 <33656314+ChenBin12138@users.noreply.github.com>
2018-08-06 09:10:22 +0800

    20180806 提交

---
+++ Assets/Editor/ChenMUShanTools.cs
@@ -0,0 +1,142 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+
+public class ChenMUShanTools :EditorWindow {
+
+    [MenuItem("陈木杉/初始化项目目录")]
+    public static void CreatNewProectDir() {
+        Debug.Log("Creat Dir !");
+
+        string datapath = Application.dataPath + "/";
+
+        if (!Directory.Exists(datapath + "Scripts"))
+            Directory.CreateDirectory(datapath + "Scripts");
+
+        if (!Directory.Exists(datapath + "Sprites"))
+            Directory.CreateDirectory(datapath + "Sprites");
+
+        if (!Directory.Exists(datapath + "Materials"))
+            Directory.CreateDirectory(datapath + "Materials");
+
+        if (!Directory.Exists(datapath + "Texutres"))
+            Directory.CreateDirectory(datapath + "Texutres");
+
+        if (!Directory.Exists(datapath + "Scenes"))
+            Directory.CreateDirectory(datapath + "Scenes");
+
+        if (!Directory.Exists(datapath + "Parfabs"))
+            Directory.CreateDirectory(datapath + "Parfabs");
+
+        if (!Directory.Exists(datapath + "Tts"))
+            Directory.CreateDirectory(datapath + "Tts");
+
+        if (!Directory.Exists(datapath + "Plugins"))
+            Directory.CreateDirectory(datapath + "Plugins");
+
+        if (!Directory.Exists(datapath + "Animations"))
+            Directory.CreateDirectory(datapath + "Animations");
+
+        if (!Directory.Exists(datapath + "Animators"))
+            Directory.CreateDirectory(datapath + "Animators");
+
+        if (!File.Exists(Application.dataPath + "/记得每日备份"))
+            File.Create(Application.dataPath + "/记得每日备份");
+
+        if (!Directory.Exists(Application.streamingAssetsPath))
+            Directory.CreateDirectory(Application.streamingAssetsPath);
+
+        Debug.Log("Creat Dir Over");
+    }
+
+    [MenuItem("陈木杉/testbtn")]
+    public static void testfun() {
+        GameObject[] objs = Selection.gameObjects;
+
+        foreach (var item in objs)
+        {
+
+            for (int i = 0; i < item.transform.childCount; i++)
+            {
+                item.transform.GetChild(i).name = i.ToString();
+                //item.transform.Find("Text").GetComponent<Text>().text = "";
+            }
+        }
+    }
+
+
+
+    [MenuItem("陈木杉/mywinodws")]
+    public static void ShowWindows() {
+        ChenMUShanTools windows = (ChenMUShanTools)EditorWindow.GetWindow(typeof(ChenMUShanTools));
+       
+
+        uIContentarr = new GUIContent[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            uIContentarr[i] = new GUIContent(i.ToString());
+        }
+        windows.Show();
+
+    }
+
+    bool is_open;
+    static GUIContent[] uIContentarr;
+    int index = 0;
+
+    void OnGUI() {
+
+
+        //EditorGUIUtility.b
+        GUILayout.Label("bbb !");
+
+
+        is_open = EditorGUILayout.BeginToggleGroup("ahhaha", is_open);
+
+        
+        index = EditorGUILayout.Popup(index, uIContentarr);
+
+
+        switch (index)
+        {
+            case 0:
+                GUILayout.Label("0~");
+                GUILayout.HorizontalSlider(5, 0, 5);
+                break;
+            case 1:
+                GUILayout.Label("1~");
+                GUILayout.HorizontalSlider(6, -6, 0);
+                break;
+            case 2:
+                GUILayout.Label("2~");
+                GUILayout.HorizontalSlider(90, -45, 45);
+                break;
+            case 3:
+                GUILayout.Label("3~");
+                GUILayout.HorizontalSlider(52, 0, 5);
+                break;
+            case 4:  break;
+            case 5:  break;
+        }
+
+
+        if (is_open)
+        {
+            GUILayout.Label("hahhah !");
+        }
+      
+
+       EditorGUILayout.EndToggleGroup();
+
+
+
+        EditorGUI.BeginChangeCheck();
+
+
+    }
+
+
+}
--- Assets/Scripts/LinkServer.cs
+++ Assets/Scripts/LinkServer.cs
@@ -1,16 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LinkServer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static LinkServer _instance;
+
+    public static string serverip = "http://open.douyucdn.cn/api/RoomApi";      //地址
+
+    public static LinkServer Instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start() {
+
+    }
+
+    /// <summary>
+    /// 获取直播房间列表信息
+    /// </summary>    
+    /// <param name="_liveName">分类ID或别名</param>
+    /// <param name="callback">回调</param>
+    /// <param name="_offset">页数偏移</param>
+    /// <param name="_limit">获取数量</param>
+    public static void GetLiveList(string _liveName,Action<string> callback,int _offset = 0, int _limit = 30){
+        string url = serverip + "/" + _liveName + "?offset" + _offset + "&limit" + _limit;
+
+        if (Instance != null) {   Instance.GetData(url, callback); }
+        else { if (callback != null) callback(null);  }
+    }
+
+    /// <summary>
+    /// 获取直播分类信息
+    /// </summary>
+    /// <param name="type"></param>
+    public static void GetLiveClassify(string type, Action<string> callback) {
+        string url = serverip + "/" + type;
+        if (Instance != null) { Instance.GetData(url, callback); }
+        else { if (callback != null) callback(null); }
+    }
+
+
+
+
+    public void GetData(string url, Action<string> callback) {
+        StartCoroutine(IGetData(url, callback));
+    }
+
+    /// <summary>
+    /// 请求接口
+    /// </summary>
+    /// <param name="url">请求路径</param>
+    /// <param name="callback">成功回调</param>
+    /// <returns></returns>
+    private IEnumerator IGetData(string url,Action<string> callback) {
+        WWW www = new WWW(url);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            if (callback != null)
+                callback(www.text);
+        }
+        else {
+            if (callback != null)
+                callback(null);
+
+            Debug.LogError("Error : " + www.error);
+        }
+    }
+
+
 }
